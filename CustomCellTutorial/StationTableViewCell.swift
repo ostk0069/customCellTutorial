98dc542e2cccf371b333a3864629e47425fabc29
Takuma Osada <judgment9ttk069@gmail.com>
2019-02-17 18:25:50 +0900

    init files

---
+++ CustomCellTutorial/StationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StationTableViewCell.swift
+//  CustomCellTutorial
+//
+//  Created by Takuma Osada on 2019/02/17.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import UIKit
+
+class StationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CustomCellTutorial/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CustomCellTutorial
+//
+//  Created by Takuma Osada on 2019/02/17.
+//  Copyright © 2019 Takuma Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var stationList: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
